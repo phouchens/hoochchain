c1fe2689604685ae1f1771cb3bec61e3e3e220b4
phouchens <perry.houchens@gmail.com>
2021-11-03 22:47:06 -0400

    refactor

--- src/main/java/com/hoochchain/Crypt.java
+++ src/main/java/com/hoochchain/Crypt.java
@@ -1,6 +1,11 @@
 package com.hoochchain;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.Optional;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Crypt {
     private Crypt() {
@@ -9,19 +14,14 @@ public class Crypt {
     public static String sha256(final String input) {
         try {
             final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            final byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            final StringBuffer hexString = new StringBuffer();
-            for (int i = 0; i < hash.length; i++) {
-                final String hex = Integer.toHexString(0xff & hash[i]);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-            return hexString.toString();
+            final byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            final IntStream hashAsStream = IntStream.range(0, hash.length).map(idx -> hash[idx]);
+            return hashAsStream.mapToObj(convertIntToStream).collect(Collectors.joining(""));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    private static final IntFunction<String> convertIntToStream = h -> Optional.of(Integer.toHexString(0xff & h)).filter(x -> x.length() != 1).orElse("0");
 
 }
